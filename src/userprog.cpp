94119c149ce452f23c609521a0f3bf3632aa21ab
himanshu <himanshu.224a@gmail.com>
2013-04-08 02:49:16 +0530

    kv to kmv

--- src/userprog.cpp
+++ src/userprog.cpp
@@ -9,19 +9,24 @@ int main(int argc, char **argv)
 
 MapReduce<int,int> mr= MapReduce<int,int>(argc,argv,8);
 mr.map(argc,argv,func);
-//KeyValue<int,int> kv = KeyValue<int,int>();
-mr.addkv(5,10);
+KeyValue<int,int> kv = KeyValue<int,int>();
+/*mr.addkv(5,10);
 mr.addkv(123,12);
 mr.addkv(351,15);
 mr.addkv(83292,7);
-//kv.printkv();
-mr.finalisemap();
+kv.printkv();
+mr.finalisemap();*/
 return 0;
 }
 
 void func(vector<primaryKV> &kv, MapReduce<int,int> *mr)
 {
-    //mr->addkv(5,10);
-    //mr->addkv(213112,2121);
-    //mr->addkv(13214,123);
+    mr->addkv(5,10);
+    mr->addkv(213112,2121);
+    mr->addkv(13214,123);
+}
+
+void rfunc(MapReduce<int,int> *mr)
+{
+    
 }
